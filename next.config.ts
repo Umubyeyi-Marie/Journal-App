5c1aa650ec151ec9a6566c61f0593f8ca6805044
Umubyeyi-Marie <mariebienvenueu@gmail.com>
2025-06-02 11:47:32 +0200

    new update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-      ignoreDuringBuilds: true,
-
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
